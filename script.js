c885ed41251709d5fcb245013929b99b6bd921dd
Xusiel <stahayb@gmail.com>
2025-04-08 18:03:57 +0330

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.mineral-card').forEach(card => {
-    card.addEventListener('click', () => {
-        alert('Mineral details: ' + card.querySelector('h3').textContent);
-    });
-});
